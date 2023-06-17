462a2b07c5b82217203c9a94ef2d22082a9d6ee9
SHewitt95 <hewitt.sherman@gmail.com>
2023-06-17 08:46:49 -0400

    start from scratch

--- main.cpp
+++ main.cpp
@@ -8,46 +8,52 @@ People (18+) can request from or send money to each other's Accounts.
 People (18+) can approve or deny transaction requests for Accounts they've made on others' behalf.
 The Bank can approve or deny transaction requests in a Queue.
 
+================================================
+Phase 1
+
 - Welcome to the Bank! What would you like to do?
-* [0][0] Login with User ID
+* [0][0] Login with User ID -> [1][0]
 * [0][1] Create User ID
-...
-* [1][0] Create an Account
-* [1][1] Deposit money
-* [1][2] Withdraw money
-* [1][3] Send money
-* [1][4] Request money
-* [1][5] View money requests (0) -> [2][0]
-* [1][6] View Accounts
-...
-* [2][0] Handle request
-...
-* [3][0] Deny request
-* [3][1] Approve request
+-- Increment ID # from last ID, log new ID, welcome user, then [2][0]
 
-========================================================
+================================================
+Phase 2
+* [1][0] Enter your User ID
+-- If ID exists, [2][0]. Else, log error message and [0][0].
 
-[1][0]
-- Enter your numeric ID:
--- If ID exists, append account. Else, associate ID with a new account.
--- Returns numeric ID of new account.
+================================================
+Phase 3
+* [2][0] Create an Account
+* [2][1] Deposit money
+* [2][2] Withdraw money
+* [2][3] Send money
+* [2][4] Request money
+* [2][5] View money requests (0)
+* [2][6] View Accounts
 
-[1][1] Enter your deposit:
--- Input a double
-
-[1][2] Enter your withdrawl amount:
--- If withdrawl < account, withdraw. Else, deny withdrawl.
-
-[1][3] Enter target account ID:
--- If account ID valid, proceed. Else, exit.
--- If send < account, send the money. Else, deny send.
-
-[1][4] Enter requested account ID:
--- If account ID valid, proceed. Else, exit.
--- Send request to account ID
-
-[1][5] Here are pending requests
--- Show names of people requesting money.
--- Show submenu [2][0]
+================================================
+Phase N
 
 */
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+
+    cout << "Welcome to the Bank! What would you like to do?\n";
+    
+    int input = 0;
+    const int SENTINEL = -1;
+    while (input > SENTINEL) {
+        cout << "[0] Login with User ID\n";
+        cout << "[1] Create User ID\n\n";
+        cout << "Input: ";
+        cin >> input;
+    }
+
+
+    return 0;
+}
